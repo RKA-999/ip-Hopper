4dd9eee9d7bd07b03ab160a30c0c0e9e470c6562
R@KA <rahitkumaradhekary161369@gmail.com>
2026-02-13 16:05:36 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 🔧 ip-Hopper Setup Script
+# Created by R.K.A
+
+echo -e "\e[1;33m[+] Updating & Upgrading Termux Packages...\e[0m"
+apt update -y && apt upgrade -y
+
+echo -e "\e[1;33m[+] Installing tur-repo & Required Packages...\e[0m"
+apt install tur-repo -y
+apt install tor privoxy netcat-openbsd curl -y
+
+echo -e "\e[1;32m[✓] All dependencies installed successfully!\e[0m"
